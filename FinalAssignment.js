1f01eed652f67ed422a1683053e97241fbcfc5a6
lrn-w <111025905+lrn-w@users.noreply.github.com>
2025-03-24 13:34:47 -0500

    Add files via upload

---
+++ FinalAssignment.js
@@ -0,0 +1,4 @@
+let person = prompt("Enter your name:");
+var greeting = "Hi " + person + ", welcome to my page! Here's a list of some of my favorite sites for programming and web development content.";
+
+document.getElementById('greeting').innerHTML = greeting;
